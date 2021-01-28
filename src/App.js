8e4b8804752b525ce31df8a7b53d3717a58534df
hiro-bata <kawa.060313@gmail.com>
2021-01-29 07:37:30 +0900

    form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import defaultDataset from "./dataset"
 import "./assets/styles/style.css"
 import {AnswersList, Chats} from "./components/index"
+import FormDialog from "./components/Forms/FormDialog"
 
 export default class App extends React.Component {
 
@@ -14,7 +15,10 @@ export default class App extends React.Component {
       dataset: defaultDataset,
       open: false
     }
+
     this.selectAnswer = this.selectAnswer.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+    this.handleClickOpen = this.handleClickOpen.bind(this)
   }
 
   displayNextQuestion = (nextQuestionId) => {
@@ -34,8 +38,20 @@ export default class App extends React.Component {
   selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch(true) {
       case (nextQuestionId === "init"):
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
         break;
+
+      case (nextQuestionId === "contact"):
+        this.handleClickOpen()
+        break;
+
+      case (/^https:*/.test(nextQuestionId)):
+        const a = document.createElement("a");
+        a.href = nextQuestionId;
+        a.target = "_blank";
+        a.click();
+        break;
+
       default:
         const chats = this.state.chats;
         chats.push({
@@ -47,14 +63,34 @@ export default class App extends React.Component {
           chats: chats
         })    
 
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 1000);
         break;
     }
   }
 
+  handleClickOpen = () => {
+    this.setState({
+        open: true
+    })
+  };
+
+  handleClose = () => {
+    this.setState({
+        open: false
+    })
+  };
+
+
   componentDidMount() {
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+  componentDidUpdate() {
+    const scrollArea = document.getElementById("scroll-area")
+    if(scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight
+    }
   }
 
   render() {
@@ -63,6 +99,7 @@ export default class App extends React.Component {
         <div className="c-box">
           <Chats chats={this.state.chats} />
           <AnswersList answers={this.state.answers} select={this.selectAnswer}/>
+          <FormDialog open={this.state.open} handleClose={this.handleClose} />
         </div>
       </section>
     );
