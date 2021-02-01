be28d6f2f9fed34f4aeb20098462e5287ac3b471
hiro-bata <kawa.060313@gmail.com>
2021-02-02 05:45:16 +0900

    modify

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,7 +3,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import NoProfile from "../assets/img/no-profile.png"
-import Torahack from "../assets/img/torahack.png"
+import Runningman from "../assets/img/run.png"
 
 const Chat = (props) => {
     const isQuestion = (props.type === "question");
@@ -13,7 +13,7 @@ const Chat = (props) => {
         <ListItem className={classes}>
             <ListItemAvatar>
                 {isQuestion ? (
-                    <Avatar alt="icon" src={Torahack} />
+                    <Avatar alt="icon" src={Runningman} />
                 ) : (
                     <Avatar alt="icon" src={NoProfile} />
                 )}
